ed71b055bbd3f6c683891404a930e4024d7cd37d
Eric St-Jean <esj@wwd.ca>
2011-12-01 09:36:19 -0400

    add logging, major refact that had been lost when splitting this into separate repo

--- fogpy/fogbugzapi.py
+++ fogpy/fogbugzapi.py
@@ -1,24 +1,82 @@
 from cStringIO import StringIO
+import logging
 from lxml import etree
+import threading
 import urllib
+
+l = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+class NotLoggedOnError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    def __unicode__(self):
+        return self.msg
 
 class FogBugzAPI(object):
     def __init__(self, base_url, username, password):
         self.base_url = base_url
+        self.username, self.password = username, password
         self.login(username, password)
 
     def login(self, username, password):
         resp = self.call('logon', notoken=True, email=username, 
                          password=password)
         self._token = resp.find('token').text
+        l.info('%s logon successful'%username)
 
-    def call(self, cmd, notoken=False, **kwargs):
+    def logout(self):
+        resp = self.call('logoff')
+        l.info('logged out')
+
+    def call(self, *args, **kwargs):
+        try:
+            return self._call(*args, **kwargs)
+        except NotLoggedOnError:
+            self.login(self.username, self.password)
+            return self._call(*args, **kwargs)
+
+    def _call(self, cmd, notoken=False, **kwargs):
         url_args = kwargs.copy() 
         url_args['cmd'] = cmd
         if not notoken:
             url_args['token'] = self._token
         url = self.base_url + '?' + urllib.urlencode(url_args)
-        resp = urllib.urlopen(url).read()
-        xml_resp = etree.parse(StringIO(resp))
+        l.debug('Calling ' + url)
+        resp = urllib.urlopen(url)
+        xml_resp = etree.parse(StringIO(resp.read()))
+        if resp.getcode() != 200:
+            msg = "%d error trying to do %s"%(resp.getcode(), cmd)
+            l.error(msg)
+            raise RuntimeError(msg)
+        elif xml_resp.find('error') is not None:
+            msg = "%s error: %s"%(cmd, xml_resp.find('error').text)
+            l.error(msg)
+            l.debug(etree.tostring(xml_resp))
+            if xml_resp.find('error').values()[0] == '3':
+                raise NotLoggedOnError(msg)
+            raise RuntimeError(msg)
+
         return xml_resp
+
+class FBApiObject(object):
+    """Base object for retrieving and saving stuff through the API"""
+    def __init__(self, base_url=None, username=None, password=None):
+        if base_url is None:
+            # try to get it from a global login
+            self._fbapi = thread.local().fbapi
+        else:
+            self._fbapi = FogBugzAPI(base_url, username, password)
+
+    pass
+
+class Bug(FBApiObject):
+    pass
+
+class Person(FBApiObject):
+    pass
+
+def login(base_url, username, password):
+    """Login to API globally"""
+    threading.local().fbapi = FogBugzAPI(base_url, username, password)
 
--- fogpy/timereport.py
+++ fogpy/timereport.py
@@ -1,30 +1,232 @@
 #!/usr/bin/env python
+r"""Create FogBugz time reports in CSV format
 
-import optparse
+Usage: %prog [options] arg1 arg2
+
+example:
+    ./fogpy/timereport.py -u foo@bar.com -p mypassword -o /tmp/out.xls \
+            -b https://ecometrica.fogbugz.com \
+            2011-10-29T00:00:00Z 2011-11-30T00:00:00Z
+"""
+
+import codecs
+from collections import defaultdict
+from cStringIO import StringIO
+import datetime as dt
+import iso8601
+import logging
+from lxml import etree
+from optparse import OptionParser
+import sys
+import urllib
 
 from fogpy.fogbugzapi import FogBugzAPI
 
+l = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
 settings = {
-    'base_url': 'https://ecometrica.fogbugz.com/api.asp',
-    'username': 'eric@ecometrica.org', 
-    'password':'robEatEu'
 }
 
-API_BASE_URL = 'https://ecometrica.fogbugz.com/api.asp'
+try:
+    import local_settings
+    for k, v in local_settings.__dict__.iteritems():
+        if not k.startswith('__'):
+            settings[k] = v
+except ImportError:
+    pass
+
+class DefaultDictForKey(defaultdict):
+    def __init__(self, default_factory):
+        self._default_factory = default_factory
+
+    def __missing__(self, key):
+        return self._default_factory(key)
 
 class TimeReporting(object):
     
-    def __init__(self, base_url, username, password):
-        self.fbapi = FogBugzAPI(API_BASE_URL, username, password)
+    def __init__(self, username, password, base_url, 
+                 start_date=None, end_date=None):
+        self.fbapi = FogBugzAPI(base_url, username, password)
+        self.bugs = DefaultDictForKey(self.get_buginfo)
+        self.devs = DefaultDictForKey(self.get_devinfo)
+        self.hours_perdev = DefaultDictForKey(self.get_hours_for_dev)
+        self.start_date, self.end_date = start_date, end_date
+        self.all_tags = set()
+        self.bugs_with_no_tags = set()
 
-    def list_intervals_for_bug(self, bugnum):
-        resp = self.fbapi.call(cmd='listIntervals', ixPerson=1, dtBug=1107)
-        return resp
+    def logout(self):
+        self.fbapi.logout()
+    
+    def get_devinfo(self, dev_id):
+        """Actually just gets info for all devs at once"""
+        resp = self.fbapi.call('listPeople')
+        for p in resp.find('people').iterfind('person'):
+            dev_id = int(p.find('ixPerson').text)
+            self.devs[dev_id] = {
+                'name': p.find('sFullName').text,
+                'email': p.find('sEmail').text
+            }
+        return self.devs[dev_id]
+
+    def get_buginfo(self, bug_id):
+        """Fill in info for a bug"""
+        resp = self.fbapi.call('search', q=`bug_id`, cols='tags,sTitle,ixBug')
+        c = resp.find('cases').find('case')
+        bug_id = int(c.find('ixBug').text)
+        self.bugs[bug_id] = {
+            'title': c.find('sTitle').text,
+            'tags': [t.text for t in c.find('tags').findall('tag')]
+        }
+        self.all_tags.update(self.bugs[bug_id]['tags'])
+        if not self.bugs[bug_id]['tags']:
+            self.bugs_with_no_tags.add(bug_id)
+        return self.bugs[bug_id]
+
+    def get_hours_for_dev(self, dev_name):
+        self.get_all_hours_per_tag_per_dev(self.start_date, self.end_date)
+        return self.hours_perdev[dev_name]
+
+    def get_all_hours_per_tag_per_dev(self, start=None, end=None):
+        if start is None: start = self.start_date
+        if end is None: end = self.end_date
+        self.hours_perdev = defaultdict(lambda: defaultdict(int))
+
+        # Find all timesheet hours
+        intervals = self._get_intervals_in_daterange(start, end)
+        for i in intervals:
+            try:
+                self._parse_interval(i)
+            except Exception, e:
+                l.error('Problem with interval: ' + etree.tostring(i))
+                raise e
+
+        # now add non-timesheet elapsed time for bugs resolved in that
+        # period, using resolvedby as dev
+        resp = self.fbapi.call(
+            'search', q='resolved:"%s..%s"'%(start.strftime('%m/%d/%Y'),
+                                             end.strftime('%m/%d/%Y')),
+            cols='ixBug,ixPerson,hrsElapsedExtra,tags,ixPersonResolvedBy',
+        )
+        for b in resp.find('cases').iterfind('case'):
+            bug_id = int(b.find('ixBug').text)
+            dev_name = self.devs[int(b.find('ixPersonResolvedBy').text)]['name']
+            hours = float(b.find('hrsElapsedExtra').text)
+            tags = self.bugs[bug_id]['tags']
+            for t in tags:
+                self.hours_perdev[dev_name][t] += hours
+            if not tags:
+                self.hours_perdev[dev_name]['None'] += hours
+            self.hours_perdev[dev_name]['total'] += hours
+            self.hours_perdev[dev_name]['non-timesheet'] += hours
+
+        if self.bugs_with_no_tags:
+            l.warning(u"Some bugs covered by this timesheet have no "
+                      u"associated tags: " 
+                      + ', '.join(`b` for b in self.bugs_with_no_tags))
+        return self.hours_perdev
+
+    def _parse_interval(self, i):
+        hours = (
+            iso8601.parse_date(i.find('dtEnd').text)
+            - iso8601.parse_date(i.find('dtStart').text) 
+        ).total_seconds() / 3600.
+        dev_name = self.devs[int(i.find('ixPerson').text)]['name']
+        tags = self.bugs[int(i.find('ixBug').text)]['tags']
+        for t in tags:
+            self.hours_perdev[dev_name][t] += hours
+        if not tags:
+            self.hours_perdev[dev_name]['None'] += hours
+        self.hours_perdev[dev_name]['total'] += hours
+
+    def _get_intervals_in_daterange(self, start, end):
+        resp = self.fbapi.call(cmd='listIntervals', ixPerson=1, 
+                               dtStart=start.isoformat()+'Z', 
+                               dtEnd=end.isoformat()+'Z')
+        return resp.find('intervals').iterfind('interval')
+    
+    def csv_hours(self):
+        if not self.hours_perdev:
+            self.get_all_hours_per_tag_per_dev()
+
+        # we want to show the columns in tag alphabetical order, with 
+        # none as the first column, and total as the last
+        tags = sorted(self.all_tags.copy())
+        if 'None' in tags: tags.remove('None')
+        tags.insert(0, 'None')
+        if 'total' in tags: tags.remove('total')
+        tags.append('total')
+        if 'non-timesheet' in tags: tags.remove('non-timesheet')
+        tags.append('non-timesheet')
+        lines = []
+        lines.append('dev name\t' + '\t'.join(tags) + '\n')
+        for k, v in self.hours_perdev.iteritems():
+            lines.append(k + '\t' + '\t'.join(`v[t]` for t in tags))
+
+        lines.append('')
+        if self.bugs_with_no_tags:
+            lines.append('Bugs with no tags:' + '\t' 
+                         + ' '.join(`b` for b in self.bugs_with_no_tags))
+            lines.append('Equivalent fogbugz filter:' + '\tOrderBy:Project ' 
+                         + ' OR '.join('ixbug:%s'%b 
+                                       for b in self.bugs_with_no_tags))
+        else:
+            lines.append('Bugs with no tags:\tnone' )
+        lines.append('')
+        lines.append('Notes')
+        lines.append("\tDon't count non-timesheet time")
+        lines.append("\tSome hours count in multiple tags, so total is less "
+                     "than the sum of tags")
+        return '\n'.join(lines)
 
 
 if __name__=='__main__':
-    tr = TimeReporting(settings['base_url'], settings['username'], 
-                       settings['password'])
-    resp = tr.list_intervals_for_bug(1231)
+    usage = __doc__
+    parser = OptionParser(usage=usage)
+    parser.add_option("-u", "--username", dest="username",
+                      help="username to log into fogbugz [%default]",
+                      default=settings.get('username', ''))
+    parser.add_option("-p", "--password", dest="password",
+                      help="password to log into fogbugz[%default] ",
+                      default=settings.get('password', ''))
+    parser.add_option("-o", "--output", dest="outfile", metavar="FILE",
+                      help="output TSV data to file instead of stdout")
+    parser.add_option("-b", "--baseurl", dest="base_url",
+                      help="Base URL for FogBugz API [%default]",
+                      default=settings.get('base_url', ''))
+
+    (options, args) = parser.parse_args()
+
+    errors = 0
+    if len(args) < 2:
+        l.error("You need to provide start and end date, in iso8601 format")
+        parser.print_help
+        errors += 1
+    if options.username is None:
+        l.error("No username given")
+        errors += 1
+    if options.password is None:
+        l.error("No password given")
+        errors += 1
+    if errors:
+        parser.print_help()
+        sys.exit(1)
+
+    start_date = iso8601.parse_date(args[0])
+    end_date = iso8601.parse_date(args[1])
+
+    #import ipdb; ipdb.set_trace()
+    tr = TimeReporting(options.username, options.password,
+                       options.base_url, start_date, end_date)
+    try:
+        hours = tr.get_all_hours_per_tag_per_dev()
+        csv_hours = tr.csv_hours()
+    finally:
+        tr.logout()
+
+    if options.outfile:
+        codecs.open(options.outfile, 'w', 'utf8').write(csv_hours)
+    else:
+        print hours
 
 
